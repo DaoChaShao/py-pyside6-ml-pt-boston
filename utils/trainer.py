27273f791ed4776bea9798ff9b39fcd591b8dc07
DaoChaShao <27882272@qq.com>
2025-10-25 16:13:05 +0800

    feat: enhance validation process with MAE calculation and progress tracking

--- utils/trainer.py
+++ utils/trainer.py
@@ -6,8 +6,9 @@
 # @File     :   trainer.py
 # @Desc     :   
 
-from torch import nn, no_grad, save, device, Tensor
+from torch import nn, no_grad, save, device, abs as torch_abs
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from utils.PT import get_device, TorchDataLoader
 
@@ -15,13 +16,13 @@ from utils.PT import get_device, TorchDataLoader
 class TorchTrainer:
     """ Trainer class for managing training process """
 
-    def __init__(self, model: nn.Module, optimiser, criterion, accelerator: str = "auto") -> None:
+    def __init__(self, model: nn.Module, optimiser, criterion, accelerator: str = "auto"):
         super().__init__()
         """ Initialise the Trainer class
         :param model: the neural network model to be trained
         :param optimiser: the optimiser for updating model parameters
         :param criterion: the loss function
-        :param accelerator: device to use for training ("cpu", "cuda", or "auto
+        :param accelerator: device to use for training ("cpu", "cuda", or "auto)
         """
         self._model = model
         self._optimiser = optimiser
@@ -46,21 +47,8 @@ class TorchTrainer:
             features, labels = features.to(device(self._accelerator)), labels.to(device(self._accelerator))
 
             self._optimiser.zero_grad()
-            outputs, _ = self._model(features)
+            outputs = self._model(features)
             # print(outputs.shape, labels.shape)
-            """
-            The expected shape using nn.CrossEntropyLoss is:
-            - input: (N, C, d1, d2, ...) N = batch_size, C = num_classes, 
-            - target: (N, d1, d2, ...)
-            The current output shape is:
-            - torch.Size([32, 12, 2509]) torch.Size([32, 12])
-            So we need to transpose the output tensor to match the expected shape:
-            - output.transpose(1, 2): [32, 2509, 12] = [N, C, seq_len]
-            - y: [32, 12] = [N, seq_len]
-            Matching perfectly!
-            """
-
-            outputs = outputs.transpose(1, 2)
             loss = self._criterion(outputs, labels)
             loss.backward()
             self._optimiser.step()
@@ -72,7 +60,7 @@ class TorchTrainer:
 
         return _loss / _total
 
-    def _epoch_valid(self, dataloader: DataLoader | TorchDataLoader) -> tuple[float, float]:
+    def _epoch_valid(self, dataloader: DataLoader | TorchDataLoader):
         """ Validate the model for one epoch
         :param dataloader: DataLoader for validation data
         :return: average validation loss for the epoch
@@ -81,44 +69,27 @@ class TorchTrainer:
         self._model.eval()
 
         _loss: float = 0.0
-        _correct: float = 0.0
         _total: float = 0.0
+        _mae: float = 0.0
         with no_grad():
             for i, (features, labels) in enumerate(dataloader):
                 features, labels = features.to(device(self._accelerator)), labels.to(device(self._accelerator))
 
-                outputs, _ = self._model(features)
+                outputs = self._model(features)
                 # print(outputs.shape, labels.shape)
-                """
-                The expected shape using nn.CrossEntropyLoss is:
-                - input: (N, C, d1, d2, ...) N = batch_size, C = num_classes, 
-                - target: (N, d1, d2, ...)
-                The current output shape is:
-                - torch.Size([32, 12, 2509]) torch.Size([32, 12])
-                So we need to transpose the output tensor to match the expected shape:
-                - output.transpose(1, 2): [32, 2509, 12] = [N, C, seq_len]
-                - y: [32, 12] = [N, seq_len]
-                Matching perfectly!
-                """
-
-                outputs = outputs.transpose(1, 2)
                 loss = self._criterion(outputs, labels)
-
                 _loss += loss.item() * features.size(0)
-                _correct += self._get_accuracy(outputs, labels)
+
+                # Calculate MAE
+                _mae += torch_abs(outputs - labels).sum().item()
+
                 _total += labels.numel()
 
                 # print(
-                #     f"Batches [{i + 1}/{len(dataloader)}] - Valid Loss: {_loss / _total:.4f} - Accuracy: {_correct / _total:.2%}"
+                #     f"Batches [{i + 1}/{len(dataloader)}] - Valid Loss: {_loss / _total:.4f}"
                 # )
 
-        return _loss / _total, _correct / _total
-
-    @staticmethod
-    def _get_accuracy(outputs: Tensor, labels: Tensor) -> float:
-        """ Get accuracy of the model """
-        predictions = outputs.argmax(dim=1)
-        return predictions.eq(labels).sum().item()
+        return _loss / _total, _mae / _total
 
     def fit(self,
             train_loader: DataLoader | TorchDataLoader, valid_loader: DataLoader | TorchDataLoader,
@@ -133,18 +104,18 @@ class TorchTrainer:
         """
         _best_valid_loss = float("inf")
 
-        for epoch in range(epochs):
+        for epoch in tqdm(range(epochs), desc="Training Progress", unit="epoches"):
             train_loss = self._epoch_train(train_loader)
-            valid_loss, accuracy = self._epoch_valid(valid_loader)
+            valid_loss, mae = self._epoch_valid(valid_loader)
 
             # self._train_losses.append(train_loss)
             # self._valid_losses.append(valid_loss)
             # self._accuracies.append(accuracy)
 
-            print(f"Epoch [{epoch + 1}/{epochs}] - "
-                  f"Train Loss: {train_loss:.4f} - "
-                  f"Valid Loss: {valid_loss:.4f} - "
-                  f"Accuracy: {accuracy:.2%}")
+            print(f"Epoch [{epoch + 1}/{epochs}] | "
+                  f"Train Loss: {train_loss:.4f} | "
+                  f"Valid Loss: {valid_loss:.4f} | "
+                  f"Valid MAE: {mae:.4f}")
 
             # Save the model if it has the best validation loss so far
             if valid_loss < _best_valid_loss:
